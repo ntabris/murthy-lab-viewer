4ce14c2c471ac30528ca49d6355c6d9487622197
Nat Tabris <nat.tabris@gmail.com>
2019-03-23 22:06:44 -0400

    Cosmetic changes to make pylint happier

--- imagefile.py
+++ imagefile.py
@@ -4,19 +4,20 @@ import h5py
 from PIL import Image
 
 class ImageFile():
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.doCaching = False # caching images doesn't seem to improve speed
+        self.frameCache = None
         self.filename = filename
         self.loadData()
 
         self.colors = [
-                        (248,240,124),
-                        (248,48,60),
-                        (168,56,44),
-                        (242,50,138),
-                        (30,82,182),
-                        (4,150,100)
-                    ]
+                        (248, 240, 124),
+                        (248, 48, 60),
+                        (168, 56, 44),
+                        (242, 50, 138),
+                        (30, 82, 182),
+                        (4, 150, 100)
+            ]
 
     def loadData(self):
         self.file = h5py.File(self.filename, "r")
@@ -27,20 +28,20 @@ class ImageFile():
     def clearCache(self):
         self.frameCache = [None] * (self.frameMax+1)
 
-    def getFrame(self,i,showOverlay = True):
+    def getFrame(self, i, showOverlay=True):
         if i < 0 or i > self.frameMax:
             return None
         # if caching is turned off, just return frame
         if not self.doCaching:
-            return self.getUncachedFrame(i,showOverlay)
+            return self.getUncachedFrame(i, showOverlay)
         
         # otherwise, put frame into cache if it isn't
         if self.frameCache[i] is None:
-            self.frameCache[i] = self.getUncachedFrame(i,showOverlay)
+            self.frameCache[i] = self.getUncachedFrame(i, showOverlay)
         # return cached frame
         return self.frameCache[i]
         
-    def getUncachedFrame(self,i,showOverlay):
+    def getUncachedFrame(self, i, showOverlay):
         test_frame_over = self.file["confmaps"][i]
         test_frame_raw = self.file["box"][i][0]
 
@@ -53,13 +54,13 @@ class ImageFile():
         img.putalpha(255)
         
         if showOverlay:
-            for channel in range(min(test_frame_over.shape[0],len(self.colors))):
+            for channel in range(min(test_frame_over.shape[0], len(self.colors))):
                 alpha = (test_frame_over[channel] * 255).astype(np.uint8)
                 r = (test_frame_over[channel] * self.colors[channel][0]).astype(np.uint8)
                 g = (test_frame_over[channel] * self.colors[channel][1]).astype(np.uint8)
                 b = (test_frame_over[channel] * self.colors[channel][2]).astype(np.uint8)
             
-                over_arr = np.dstack((b,g,r,alpha))
+                over_arr = np.dstack((b, g, r, alpha))
         
                 over = Image.fromarray(over_arr)
                 img = Image.alpha_composite(img, over)
@@ -68,9 +69,9 @@ class ImageFile():
         return img_arr
         
     def findCenterOfMass(self, arr, a=0, b=-1):
-        if type(arr) is int:
+        if isinstance(arr, int):
             over_flat = self.file["confmaps"][arr].sum(axis=0)
-            return self.findCenterOfMass(over_flat,0,-1)
+            return self.findCenterOfMass(over_flat, 0, -1)
     
         # if given two dimensional array, flatten and calculate along each axis
         if len(arr.shape) == 2:
