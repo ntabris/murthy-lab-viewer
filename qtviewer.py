7af7e84b35891fa65d698517d1bf4923afce241c
Nat Tabris <nat.tabris@gmail.com>
2019-03-25 07:19:39 -0400

    Wrap window contents into QMainWindow to support scrolling and dock. Added dock with buttons.

--- qtviewer.py
+++ qtviewer.py
@@ -5,10 +5,11 @@ from imagefile import ImageFile
 from PySide2 import QtGui, QtCore
 from PySide2.QtCore import Qt, Slot, QRect
 from PySide2.QtGui import QPixmap, QImage
-from PySide2.QtWidgets import (QAction, QApplication, QPushButton, QVBoxLayout, 
+from PySide2.QtWidgets import (QAction, QApplication, QPushButton, QHBoxLayout,
+                               QMainWindow, QDockWidget, QWidget,
                                QGraphicsView, QGraphicsScene, QGraphicsPixmapItem)
 
-class ms_ImageViewer(QGraphicsView):
+class ms_ImageViewer(QMainWindow):
 
     def __init__(self,filename):
         super(ms_ImageViewer, self).__init__()
@@ -33,25 +34,39 @@ class ms_ImageViewer(QGraphicsView):
             self.frameIdx += 1
             self.updateUI()
     
+    def firstFrame(self):
+        self.frameIdx = 0
+        self.updateUI()
+
+    def lastFrame(self):
+        self.frameIdx = self.imageFile.frameMax
+        self.updateUI()
+    
+    def toggleZoom(self):
+        self.doZoom = not self.doZoom
+        self.updateUI()
+    
+    def toggleOverlay(self):
+        self.showOverlay = not self.showOverlay
+        # don't recenter since we're not changing the frame
+        self.updateUI(updateCenter=False)
+    
     def keyPressEvent(self, e):
-        # left arrow goes to previous frame
-        if e.key() == 16777234:
+        # left arrow (or J) goes to previous frame
+        if e.key() == 16777234 or e.key() == 74:
             self.prevFrame()
-        # right arrow goes to next frame
-        elif e.key() == 16777236:
+        # right arrow (or K) goes to next frame
+        elif e.key() == 16777236 or e.key() == 75:
             self.nextFrame()
-        # up arrow goes to first frame
-        elif e.key() == 16777235:
-            self.frameIdx = 0
-            self.updateUI()
-        # down arrow goes to last frame
-        elif e.key() == 16777237:
-            self.frameIdx = self.imageFile.frameMax
-            self.updateUI()
+        # up arrow (or H) goes to first frame
+        elif e.key() == 16777235 or e.key() == 72:
+            self.firstFrame()
+        # down arrow (or L) goes to last frame
+        elif e.key() == 16777237 or e.key() == 76:
+            self.lastFrame()
         # tab key toggles overlay
         elif e.key() == 16777217:
-            self.showOverlay = not self.showOverlay
-            self.updateUI()
+            self.toggleOverlay()
         # enter key goes to number entered
         elif e.key() == 16777220:
             if self.goTo >= 0 and self.goTo <= self.imageFile.frameMax:
@@ -65,23 +80,23 @@ class ms_ImageViewer(QGraphicsView):
                 self.goTo = 10 * self.goTo + int(chr(e.key()))
             # z toggles zoom
             elif chr(e.key()) == 'Z':
-                self.doZoom = not self.doZoom
-                self.updateUI()
-        else:
-            #print("key:",e.key())
-            pass
+                self.toggleZoom()
+            else:
+                #print("key:",e.key())
+                pass
     
-    def updateUI(self):
+    def updateUI(self, updateCenter=True):
         self.pixmap.setPixmap(self.getFramePixmap(self.frameIdx))
         outlineBox = self.imageFile.box(self.imageFile.findCenterOfMass(self.frameIdx))
         if self.boxRect: self.boxRect.setRect(*outlineBox)
         #self.translate(-outlineBox[0],-outlineBox[1])
         if self.doZoom:
-            self.resetTransform()
-            self.scale(2,2)
-            self.centerOn(*self.imageFile.findCenterOfMass(self.frameIdx))
+            self.view.resetTransform()
+            self.view.scale(2,2)
+            if updateCenter:
+                self.view.centerOn(*self.imageFile.findCenterOfMass(self.frameIdx))
         else:
-            self.resetTransform()
+            self.view.resetTransform()
         self.setWindowTitle("%s... [%d/%d]"%
             (self.filename[:20],self.frameIdx,self.imageFile.frameMax))
     
@@ -94,9 +109,7 @@ class ms_ImageViewer(QGraphicsView):
         return QtGui.QPixmap.fromImage(image)
     
     def initUI(self):
-        self.main_layout = QVBoxLayout()
-
-        self.scene = QGraphicsScene(self.main_layout)
+        self.scene = QGraphicsScene()
         self.pixmap = QGraphicsPixmapItem()
         self.scene.addItem(self.pixmap)
         
@@ -109,17 +122,52 @@ class ms_ImageViewer(QGraphicsView):
             self.scene.addRect(QRect(*outlineBox),pen=QtGui.QPen(QtCore.Qt.blue, 1))
             self.boxRect = self.scene.items()[0] # not the best way to get rect
         
-        self.setScene(self.scene)
+        self.view = QGraphicsView()
+        self.view.setDragMode(QGraphicsView.ScrollHandDrag)
+        self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        #self.view.setFixedSize(256,256) # ???
+        #self.view.setGeometry(20, 20, self.imageFile.imgW, self.imageFile.imgH)
+        self.view.setScene(self.scene)
         
-#         prevButton = QPushButton('previous', self)
-#         prevButton.clicked.connect(self.prevFrame)
-#         self.main_layout.addWidget(prevButton)
-#         nextButton = QPushButton('next', self)
-#         nextButton.clicked.connect(self.nextFrame)
-#         self.main_layout.addWidget(nextButton)
-#         self.setLayout(self.main_layout)
+        self.setCentralWidget(self.view)
         
-        self.setGeometry(300, 300, self.imageFile.imgH, self.imageFile.imgW)
+        controlDock = QDockWidget()
+        controlDock.setFeatures(QDockWidget.NoDockWidgetFeatures)
+        self.addDockWidget(Qt.TopDockWidgetArea, controlDock)
+        
+        main_layout = QHBoxLayout()
+        main_layout.setContentsMargins(0,0,0,0)
+        # first frame
+        firstButton = QPushButton('<<', self)
+        firstButton.clicked.connect(self.firstFrame)
+        main_layout.addWidget(firstButton)
+        # previous frame
+        prevButton = QPushButton('<', self)
+        prevButton.clicked.connect(self.prevFrame)
+        main_layout.addWidget(prevButton)
+        # next frame
+        nextButton = QPushButton('>', self)
+        nextButton.clicked.connect(self.nextFrame)
+        main_layout.addWidget(nextButton)
+        # last frame
+        lastButton = QPushButton('>>', self)
+        lastButton.clicked.connect(self.lastFrame)
+        main_layout.addWidget(lastButton)
+        # zoom
+        zoomButton = QPushButton('zoom', self)
+        zoomButton.clicked.connect(self.toggleZoom)
+        main_layout.addWidget(zoomButton)
+        # zoom
+        overlayButton = QPushButton('color', self)
+        overlayButton.clicked.connect(self.toggleOverlay)
+        main_layout.addWidget(overlayButton)
+        # add dock widget
+        dockContainerWidget = QWidget(controlDock)
+        dockContainerWidget.setLayout(main_layout)
+        dockContainerWidget.setGeometry(0,0,self.imageFile.imgW,20)
+        
+        self.setGeometry(320, 320, self.imageFile.imgW, self.imageFile.imgH+dockContainerWidget.height())
 
         self.show()
     
